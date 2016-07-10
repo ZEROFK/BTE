10da97b4eb1a8505e2111eee9988f0d59aa0c9fd
ZEROFK <zerofk@protonmail.ch>
2016-07-09 23:22:18 -0400

    update zh_CN

--- src/qt/locale/bitcoin_zh_CN.ts
+++ src/qt/locale/bitcoin_zh_CN.ts
@@ -4,12 +4,12 @@
     <message>
         <location filename="../forms/aboutdialog.ui" line="+14"/>
         <source>About BitExchange</source>
-        <translation>关于暗影币</translation>
+        <translation>关于比特兑</translation>
     </message>
     <message>
         <location line="+39"/>
         <source>&lt;b&gt;BitExchange&lt;/b&gt; version</source>
-        <translation>&lt;b&gt;暗影币客户端&lt;/b&gt; 版本</translation>
+        <translation>&lt;b&gt;比特兑客户端&lt;/b&gt; 版本</translation>
     </message>
     <message>
         <location line="+41"/>
@@ -18,8 +18,8 @@ Copyright © 2012-2014 The NovaCoin developers
 Copyright © 2014 The BlackCoin developers
 Copyright © 2014 The BitExchange developers</source>
         <translation>版权所有 © 2009-2014 比特币Bitcoin开发组
-版权所有 © 2012-2014 新星币Novacoin开发组
-版权所有 © 2014 暗影币BitExchange开发组</translation>
+版权所有 © 2012-2014 Novacoin开发组
+版权所有 © 2014 比特兑BitExchange开发组</translation>
     </message>
     <message>
         <location line="+15"/>
@@ -67,7 +67,7 @@ This product includes software developed by the OpenSSL Project for use in the O
     <message>
         <location line="-46"/>
         <source>These are your BitExchange addresses for receiving payments. You may want to give a different one to each sender so you can keep track of who is paying you.</source>
-        <translation>这是您用来接收支付的暗影币地址列表。为不同的支付方建立不同的地址以便于了解支付来源。</translation>
+        <translation>这是您用来接收支付的比特兑地址列表。为不同的支付方建立不同的地址以便于了解支付来源。</translation>
     </message>
     <message>
         <location line="+60"/>
@@ -82,7 +82,7 @@ This product includes software developed by the OpenSSL Project for use in the O
     <message>
         <location line="+11"/>
         <source>Sign a message to prove you own a BitExchange address</source>
-        <translation>对信息进行签名以证明您对该暗影币地址的所有权</translation>
+        <translation>对信息进行签名以证明您对该比特兑地址的所有权</translation>
     </message>
     <message>
         <location line="+3"/>
@@ -97,7 +97,7 @@ This product includes software developed by the OpenSSL Project for use in the O
     <message>
         <location line="-14"/>
         <source>Verify a message to ensure it was signed with a specified BitExchange address</source>
-        <translation>验证信息以保证其经过指定暗影币地址的签名</translation>
+        <translation>验证信息以保证其经过指定比特兑地址的签名</translation>
     </message>
     <message>
         <location line="+3"/>
@@ -238,7 +238,7 @@ This product includes software developed by the OpenSSL Project for use in the O
     <message>
         <location line="+1"/>
         <source>Warning: If you encrypt your wallet and lose your passphrase, you will &lt;b&gt;LOSE ALL OF YOUR COINS&lt;/b&gt;!</source>
-        <translation>警告：如果您丢失了加密该钱包的密码，其中所有的暗影币将会丢失！</translation>
+        <translation>警告：如果您丢失了加密该钱包的密码，其中所有的比特兑将会丢失！</translation>
     </message>
     <message>
         <location line="+0"/>
@@ -265,7 +265,7 @@ This product includes software developed by the OpenSSL Project for use in the O
     <message>
         <location line="-58"/>
         <source>BitExchange will close now to finish the encryption process. Remember that encrypting your wallet cannot fully protect your coins from being stolen by malware infecting your computer.</source>
-        <translation>暗影币客户端即将关闭以完成加密过程。请记住，加密钱包并不能完全防止您的电子货币被入侵您计算机的木马软件盗窃。</translation>
+        <translation>比特兑客户端即将关闭以完成加密过程。请记住，加密钱包并不能完全防止您的电子货币被入侵您计算机的木马软件盗窃。</translation>
     </message>
     <message>
         <location line="+13"/>
@@ -354,7 +354,7 @@ This product includes software developed by the OpenSSL Project for use in the O
     <message>
         <location line="-13"/>
         <source>&amp;Receive coins</source>
-        <translation>接收暗影币(&amp;R)</translation>
+        <translation>接收比特兑(&amp;R)</translation>
     </message>
     <message>
         <location line="+1"/>
@@ -364,7 +364,7 @@ This product includes software developed by the OpenSSL Project for use in the O
     <message>
         <location line="-7"/>
         <source>&amp;Send coins</source>
-        <translation>发送暗影币(&amp;S)</translation>
+        <translation>发送比特兑(&amp;S)</translation>
     </message>
     <message>
         <location line="+35"/>
@@ -379,7 +379,7 @@ This product includes software developed by the OpenSSL Project for use in the O
     <message>
         <location line="+4"/>
         <source>Show information about BitExchange</source>
-        <translation>显示关于暗影币的信息</translation>
+        <translation>显示关于比特兑的信息</translation>
     </message>
     <message>
         <location line="+2"/>
@@ -429,7 +429,7 @@ This product includes software developed by the OpenSSL Project for use in the O
     <message>
         <location line="-62"/>
         <source>Send coins to a BitExchange address</source>
-        <translation>向指定的地址发送暗影币</translation>
+        <translation>向指定的地址发送比特兑</translation>
     </message>
     <message>
         <location line="+45"/>
@@ -474,7 +474,7 @@ This product includes software developed by the OpenSSL Project for use in the O
     <message>
         <location line="-200"/>
         <source>BitExchange</source>
-        <translation>暗影币</translation>
+        <translation>比特兑</translation>
     </message>
     <message>
         <location line="+0"/>
@@ -484,7 +484,7 @@ This product includes software developed by the OpenSSL Project for use in the O
     <message>
         <location line="+178"/>
         <source>&amp;About BitExchange</source>
-        <translation>关于暗影币(&amp;A)</translation>
+        <translation>关于比特兑(&amp;A)</translation>
     </message>
     <message>
         <location line="+9"/>
@@ -541,12 +541,12 @@ This product includes software developed by the OpenSSL Project for use in the O
         <location line="+0"/>
         <location line="+60"/>
         <source>BitExchange client</source>
-        <translation>暗影币客户端</translation>
+        <translation>比特兑客户端</translation>
     </message>
     <message numerus="yes">
         <location line="+70"/>
         <source>%n active connection(s) to BitExchange network</source>
-        <translation><numerusform>与暗影币网络建立了 %n 个连接</numerusform></translation>
+        <translation><numerusform>与比特兑网络建立了 %n 个连接</numerusform></translation>
     </message>
     <message>
         <location line="+40"/>
@@ -576,7 +576,7 @@ This product includes software developed by the OpenSSL Project for use in the O
     <message>
         <location line="+2"/>
         <source>Not staking because you don&apos;t have mature coins</source>
-        <translation>未进行权益增值，因为钱包中没有成熟的暗影币</translation>
+        <translation>未进行权益增值，因为钱包中没有成熟的比特兑</translation>
     </message>
     <message numerus="yes">
         <location line="-403"/>
@@ -661,7 +661,7 @@ Address: %4
         <location line="-15"/>
         <location line="+15"/>
         <source>URI can not be parsed! This can be caused by an invalid BitExchange address or malformed URI parameters.</source>
-        <translation>无法解析URI：无效的暗影币地址或错误的URI参数。</translation>
+        <translation>无法解析URI：无效的比特兑地址或错误的URI参数。</translation>
     </message>
     <message>
         <location line="+18"/>
@@ -721,7 +721,7 @@ Address: %4
     <message>
         <location filename="../bitcoin.cpp" line="+109"/>
         <source>A fatal error occurred. BitExchange can no longer continue safely and will quit.</source>
-        <translation>发生严重错误，暗影币客户端即将关闭。</translation>
+        <translation>发生严重错误，比特兑客户端即将关闭。</translation>
     </message>
 </context>
 <context>
@@ -737,7 +737,7 @@ Address: %4
     <message>
         <location filename="../forms/coincontroldialog.ui" line="+14"/>
         <source>Coin Control</source>
-        <translation>暗影币控制</translation>
+        <translation>比特兑控制</translation>
     </message>
     <message>
         <location line="+31"/>
@@ -1048,7 +1048,7 @@ This label turns red, if the priority is smaller than &quot;medium&quot;.
     <message>
         <location line="-5"/>
         <source>The entered address &quot;%1&quot; is not a valid BitExchange address.</source>
-        <translation>“%1” 不是有效的暗影币地址。</translation>
+        <translation>“%1” 不是有效的比特兑地址。</translation>
     </message>
     <message>
         <location line="+10"/>
@@ -1067,7 +1067,7 @@ This label turns red, if the priority is smaller than &quot;medium&quot;.
         <location filename="../guiutil.cpp" line="+420"/>
         <location line="+12"/>
         <source>BitExchange-Qt</source>
-        <translation>暗影币客户端BitExchange-Qt</translation>
+        <translation>比特兑客户端BitExchange-Qt</translation>
     </message>
     <message>
         <location line="-12"/>
@@ -1140,12 +1140,12 @@ This label turns red, if the priority is smaller than &quot;medium&quot;.
     <message>
         <location line="+31"/>
         <source>Automatically start BitExchange after logging in to the system.</source>
-        <translation>开机自动启动暗影币客户端</translation>
+        <translation>开机自动启动比特兑客户端</translation>
     </message>
     <message>
         <location line="+3"/>
         <source>&amp;Start BitExchange on system login</source>
-        <translation>开机时自动启动暗影币客户端(&amp;S)</translation>
+        <translation>开机时自动启动比特兑客户端(&amp;S)</translation>
     </message>
     <message>
         <location line="+7"/>
@@ -1165,7 +1165,7 @@ This label turns red, if the priority is smaller than &quot;medium&quot;.
     <message>
         <location line="+6"/>
         <source>Automatically open the BitExchange client port on the router. This only works when your router supports UPnP and it is enabled.</source>
-        <translation>路由器自动打开暗影币客户端端口。该功能仅在路由器开启UPnP支持时有效。</translation>
+        <translation>路由器自动打开比特兑客户端端口。该功能仅在路由器开启UPnP支持时有效。</translation>
     </message>
     <message>
         <location line="+3"/>
@@ -1175,7 +1175,7 @@ This label turns red, if the priority is smaller than &quot;medium&quot;.
     <message>
         <location line="+7"/>
         <source>Connect to the BitExchange network through a SOCKS proxy (e.g. when connecting through Tor).</source>
-        <translation>经SOCKS代理连接到暗影币网络(例如使用Tor)</translation>
+        <translation>经SOCKS代理连接到比特兑网络(例如使用Tor)</translation>
     </message>
     <message>
         <location line="+3"/>
@@ -1250,27 +1250,27 @@ This label turns red, if the priority is smaller than &quot;medium&quot;.
     <message>
         <location line="+13"/>
         <source>The user interface language can be set here. This setting will take effect after restarting BitExchange.</source>
-        <translation>在此设置用户界面语言。重启暗影币客户端后设置生效。</translation>
+        <translation>在此设置用户界面语言。重启比特兑客户端后设置生效。</translation>
     </message>
     <message>
         <location line="+11"/>
         <source>&amp;Unit to show amounts in:</source>
-        <translation>暗影币金额单位(&amp;U)：</translation>
+        <translation>比特兑金额单位(&amp;U)：</translation>
     </message>
     <message>
         <location line="+13"/>
         <source>Choose the default subdivision unit to show in the interface and when sending coins.</source>
-        <translation>选择暗影币单位。</translation>
+        <translation>选择比特兑单位。</translation>
     </message>
     <message>
         <location line="+9"/>
         <source>Whether to show BitExchange addresses in the transaction list or not.</source>
-        <translation>是否在交易列表中显示暗影币地址</translation>
+        <translation>是否在交易列表中显示比特兑地址</translation>
     </message>
     <message>
         <location line="+3"/>
         <source>&amp;Display addresses in transaction list</source>
-        <translation>在交易清单中显示暗影币地址(&amp;D)</translation>
+        <translation>在交易清单中显示比特兑地址(&amp;D)</translation>
     </message>
     <message>
         <location line="+7"/>
@@ -1280,7 +1280,7 @@ This label turns red, if the priority is smaller than &quot;medium&quot;.
     <message>
         <location line="+3"/>
         <source>Display coin &amp;control features (experts only!)</source>
-        <translation>显示暗影币控制选项(仅用于专家用户)</translation>
+        <translation>显示比特兑控制选项(仅用于专家用户)</translation>
     </message>
     <message>
         <location line="+71"/>
@@ -1567,12 +1567,12 @@ This label turns red, if the priority is smaller than &quot;medium&quot;.
     <message>
         <location line="-104"/>
         <source>BitExchange - Debug window</source>
-        <translation>暗影币客户端-调试窗口</translation>
+        <translation>比特兑客户端-调试窗口</translation>
     </message>
     <message>
         <location line="+25"/>
         <source>BitExchange Core</source>
-        <translation>暗影币核心进程</translation>
+        <translation>比特兑核心进程</translation>
     </message>
     <message>
         <location line="+279"/>
@@ -1592,7 +1592,7 @@ This label turns red, if the priority is smaller than &quot;medium&quot;.
     <message>
         <location filename="../rpcconsole.cpp" line="-33"/>
         <source>Welcome to the BitExchange RPC console.</source>
-        <translation>欢迎使用暗影币RPC控制台。</translation>
+        <translation>欢迎使用比特兑RPC控制台。</translation>
     </message>
     <message>
         <location line="+1"/>
@@ -1751,7 +1751,7 @@ This label turns red, if the priority is smaller than &quot;medium&quot;.
     <message>
         <location filename="../sendcoinsdialog.cpp" line="-173"/>
         <source>Enter a BitExchange address (e.g. SXywGBZBowrppUwwNUo1GCRDTibzJi7g2M)</source>
-        <translation>输入暗影币地址(例如：SXywGBZBowrppUwwNUo1GCRDTibzJi7g2M)</translation>
+        <translation>输入比特兑地址(例如：SXywGBZBowrppUwwNUo1GCRDTibzJi7g2M)</translation>
     </message>
     <message>
         <location line="+15"/>
@@ -1851,7 +1851,7 @@ This label turns red, if the priority is smaller than &quot;medium&quot;.
     <message>
         <location line="+251"/>
         <source>WARNING: Invalid BitExchange address</source>
-        <translation>警告：无效的暗影币地址</translation>
+        <translation>警告：无效的比特兑地址</translation>
     </message>
     <message>
         <location line="+13"/>
@@ -1925,7 +1925,7 @@ This label turns red, if the priority is smaller than &quot;medium&quot;.
     <message>
         <location filename="../sendcoinsentry.cpp" line="+1"/>
         <source>Enter a BitExchange address (e.g. SXywGBZBowrppUwwNUo1GCRDTibzJi7g2M)</source>
-        <translation>输入一个暗影币地址 (例：SXywGBZBowrppUwwNUo1GCRDTibzJi7g2M)</translation>
+        <translation>输入一个比特兑地址 (例：SXywGBZBowrppUwwNUo1GCRDTibzJi7g2M)</translation>
     </message>
 </context>
 <context>
@@ -1986,7 +1986,7 @@ This label turns red, if the priority is smaller than &quot;medium&quot;.
     <message>
         <location line="+21"/>
         <source>Sign the message to prove you own this BitExchange address</source>
-        <translation>对该消息进行签名以证明您对该暗影币地址的所有权</translation>
+        <translation>对该消息进行签名以证明您对该比特兑地址的所有权</translation>
     </message>
     <message>
         <location line="+17"/>
@@ -2013,12 +2013,12 @@ This label turns red, if the priority is smaller than &quot;medium&quot;.
     <message>
         <location line="+21"/>
         <source>The address the message was signed with (e.g. SXywGBZBowrppUwwNUo1GCRDTibzJi7g2M)</source>
-        <translation>用来签名该消息的暗影币地址(例： SXywGBZBowrppUwwNUo1GCRDTibzJi7g2M)</translation>
+        <translation>用来签名该消息的比特兑地址(例： SXywGBZBowrppUwwNUo1GCRDTibzJi7g2M)</translation>
     </message>
     <message>
         <location line="+40"/>
         <source>Verify the message to ensure it was signed with the specified BitExchange address</source>
-        <translation>确认该消息以保证它经由指定的暗影币地址签名</translation>
+        <translation>确认该消息以保证它经由指定的比特兑地址签名</translation>
     </message>
     <message>
         <location line="+17"/>
@@ -2029,7 +2029,7 @@ This label turns red, if the priority is smaller than &quot;medium&quot;.
         <location filename="../signverifymessagedialog.cpp" line="+27"/>
         <location line="+3"/>
         <source>Enter a BitExchange address (e.g. SXywGBZBowrppUwwNUo1GCRDTibzJi7g2M)</source>
-        <translation>输入暗影币地址(例： SXywGBZBowrppUwwNUo1GCRDTibzJi7g2M)</translation>
+        <translation>输入比特兑地址(例： SXywGBZBowrppUwwNUo1GCRDTibzJi7g2M)</translation>
     </message>
     <message>
         <location line="-2"/>
@@ -2039,7 +2039,7 @@ This label turns red, if the priority is smaller than &quot;medium&quot;.
     <message>
         <location line="+3"/>
         <source>Enter BitExchange signature</source>
-        <translation>输入暗影币签名</translation>
+        <translation>输入比特兑签名</translation>
     </message>
     <message>
         <location line="+82"/>
@@ -2610,7 +2610,7 @@ This label turns red, if the priority is smaller than &quot;medium&quot;.
     <message>
         <location filename="../bitcoinstrings.cpp" line="+33"/>
         <source>BitExchange version</source>
-        <translation>暗影币客户端 版本</translation>
+        <translation>比特兑客户端 版本</translation>
     </message>
     <message>
         <location line="+1"/>
@@ -2699,7 +2699,7 @@ This label turns red, if the priority is smaller than &quot;medium&quot;.
     <message>
         <location line="+2"/>
         <source>Stake your coins to support network and gain reward (default: 1)</source>
-        <translation>进行权益累积以支持暗影币网络并获得报酬(默认: 1)</translation>
+        <translation>进行权益累积以支持比特兑网络并获得报酬(默认: 1)</translation>
     </message>
     <message>
         <location line="+5"/>
@@ -2803,7 +2803,7 @@ This label turns red, if the priority is smaller than &quot;medium&quot;.
     <message>
         <location line="+61"/>
         <source>Warning: Please check that your computer&apos;s date and time are correct! If your clock is wrong BitExchange will not work properly.</source>
-        <translation>警告：请确认您计算机的本地时间。如果时钟错误，暗影币客户端将不能正常工作。</translation>
+        <translation>警告：请确认您计算机的本地时间。如果时钟错误，比特兑客户端将不能正常工作。</translation>
     </message>
     <message>
         <location line="-31"/>
@@ -3139,12 +3139,12 @@ alertnotify=echo %%s | mail -s &quot;BitExchange Alert&quot; admin@foo.com
     <message>
         <location line="+1"/>
         <source>Cannot obtain a lock on data directory %s.  BitExchange is probably already running.</source>
-        <translation>无法从数据目录 %s 获得锁定.  暗影币客户端可能已在运行中.</translation>
+        <translation>无法从数据目录 %s 获得锁定.  比特兑客户端可能已在运行中.</translation>
     </message>
     <message>
         <location line="-98"/>
         <source>BitExchange</source>
-        <translation>暗影币</translation>
+        <translation>比特兑</translation>
     </message>
     <message>
         <location line="+140"/>
@@ -3254,7 +3254,7 @@ alertnotify=echo %%s | mail -s &quot;BitExchange Alert&quot; admin@foo.com
     <message>
         <location line="+122"/>
         <source>Unable to bind to %s on this computer. BitExchange is probably already running.</source>
-        <translation>无法绑定到该计算机上的 %s. 暗影币客户端可能已在运行中。</translation>
+        <translation>无法绑定到该计算机上的 %s. 比特兑客户端可能已在运行中。</translation>
     </message>
     <message>
         <location line="-97"/>
